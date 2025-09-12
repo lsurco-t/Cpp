e6df80a09f3b3ea085cac51bc70689ba91791225
Lucio <lucio.surcotaype@outlook.com>
2025-09-12 20:51:50 +0300

    simplified version

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -7,16 +7,12 @@ int	main(int ac, char **av)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return 0;
 	}
-	for (int i = 1; av[i]; i++){
-		for (int j = 0; av[i][j]; j++){
-			if (isalpha(av[i][j])){
-				unsigned char c = static_cast<unsigned char>(av[i][j]);
-				std::cout << static_cast<char>(std::toupper(c));
-			}
-			else
-				std::cout << av[i][j];
+	for (int i = 1; av[i]; i++) {
+		for (int j = 0; av[i][j]; j++) {
+			unsigned char c = static_cast<unsigned char>(av[i][j]);
+			std::cout << static_cast<char>(std::toupper(c));
 		}
-		std::cout << " ";
+		if (av[i + 1]) std::cout << " ";
 	}
 	std::cout << std::endl;
 	return 0;
