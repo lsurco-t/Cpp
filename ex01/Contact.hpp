94585d3865017437fe9cf7f61fd67ab4a8900415
Lucio <lucio.surcotaype@outlook.com>
2025-09-12 23:47:38 +0300

    saved changes

--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -1,4 +1,15 @@
 #pragma once
 
+#include <string>
 
+class Contact {
+	private:
+
+	public:
+		std::string 	FirstName;
+		std::string 	LastName;
+		std::string 	Nickname;
+		unsigned int	PhoneNumber;
+		std::string 	DarkestSecret;
+};
 
