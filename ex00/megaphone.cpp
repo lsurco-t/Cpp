e2be51c860bc766880891879745d4587f69f2ee4
Lucio <lucio.surcotaype@outlook.com>
2025-09-12 16:59:14 +0300

    saved progress

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cctype>
+
+int	main(int ac, char **av)
+{
+	int i = 0;
+
+	if (ac <= 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	while(av)
+	{
+		
+	}
+	return 0;
+}
