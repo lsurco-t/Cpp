5f74009e2564b93e4b262fc04af21c73d45226d4
Lucio <lucio.surcotaype@outlook.com>
2025-09-21 17:04:14 +0300

    finished ex06

--- cpp01/ex06/Harl.cpp
+++ cpp01/ex06/Harl.cpp
@@ -2,22 +2,22 @@
 
 void Harl::debug(){
 	std::cout << "[ DEBUG ]\n"
-			  << "I just spent three hours aligning my Makefile so every tab is perfect. It looks amazing, trust me.\n";
+			  << "I just spent three hours aligning my Makefile so every tab is perfect. It looks amazing, trust me.\n\n";
 }
 
 void Harl::info(){
 	std::cout << "[ INFO ]\n"
-			  << "Why does Norminette care so much about spaces? I just wanted to add one more line, and boom — error!\n";
+			  << "Why does Norminette care so much about spaces? I just wanted to add one more line, and boom — error!\n\n";
 }
 
 void Harl::warning(){
 	std::cout << "[ WARNING ]\n"
-			  << "I think I deserve some points back. The moulinette crashed during my defense!\n";
+			  << "I think I deserve some points back. The moulinette crashed during my defense!\n\n";
 }
 
 void Harl::error(){
 	std::cout << "[ ERROR ]\n"
-			  << "This is unacceptable! If my get_next_line segfaults one more time, I am dropping out of Hive!\n";
+			  << "This is unacceptable! If my get_next_line segfaults one more time, I am dropping out of Hive!\n\n";
 }
 
 void Harl::complain(std::string level){
@@ -25,11 +25,25 @@ void Harl::complain(std::string level){
 	std::string nameLevels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
 	levelFunctions arrayComplains[] = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
 
+	int index = -1;
 	for (int i = 0; i < 4; i++){
 		if (nameLevels[i] == level){
-			(this->*arrayComplains[i])();
-			return ;
+			index = i;
+			break ;
 		}
 	}
-	std::cout << "[ Probably complaining about insignificant problems ]\n";
+
+	switch (index) {
+		case 0: (this->*arrayComplains[0])();
+				//fallthrough
+		case 1: (this->*arrayComplains[1])();
+				//fallthrough
+		case 2: (this->*arrayComplains[2])();
+				//fallthrough
+		case 3: (this->*arrayComplains[3])();
+			break;
+		
+		default:
+			std::cout << "[ Probably complaining about insignificant problems ]\n";
+	}
 }
--- cpp01/ex06/main.cpp
+++ cpp01/ex06/main.cpp
@@ -6,15 +6,15 @@ int main(int argc, char **argv)
 	Harl hiverEight;
 
 	if (argc != 2){
-		std::cout << "Your complains can not be heard, enter a valid one!\n";
+		std::cout << "\033[31mYour complains can not be heard, enter a valid one!\n\033[0m";
 		return 1;
 	}
 
-	std::cout << "Hiver eight mode: ";
+	std::cout << "\033[31mHiver eight mode: \033[0m";
 	hiverEight.complain(argv[1]);
 	std::cout << "\n";
 
-	std::cout << "Hiver eight mode: ";
+	std::cout << "\033[31mHiver eight mode: \033[0m";
 	hiverEight.complain(argv[1]);
 	std::cout << "\n";
 
