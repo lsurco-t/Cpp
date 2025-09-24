c818b84072acab0d590f42e06ce61194b31531d8
Lucio <lucio.surcotaype@outlook.com>
2025-09-24 12:04:19 +0300

    saved progress

---
+++ cpp02/ex03/Point.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#include "Fixed.hpp"
+
+class Point {
+	
+	private:
+		const Fixed _x;
+		const Fixed _y;
+
+	public:
+		Point();
+		Point(const float x, const float y);
+		Point (const Point& other);
+		Point& operator=(const Point& other) = delete;
+		~Point();
+};
