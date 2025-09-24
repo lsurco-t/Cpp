109b20d0b101b4f2a6cccb5d6edc3a7d5f5634fc
Lucio <lucio.surcotaype@outlook.com>
2025-09-24 15:42:41 +0300

    saved progress

--- cpp02/ex03/main.cpp
+++ cpp02/ex03/main.cpp
@@ -6,8 +6,20 @@ bool bsp( Point const a, Point const b, Point const c, Point const point);
 
 int main (void){
 
-	// if (bsp(4, 5, 6, 3) == true)
-	// 	std::cout "Point is inside triangle!\n";
+	Point a(0.0f, 0.0f);
+	Point b(10.0f, 0.0f);
+	Point c(5.0f, 10.25f);
 	
+	Point p(4.0f, 5.0f);
+
+	std::cout << "A values: " << a.getX() << ", " << a.getY() << std::endl;
+	std::cout << "B values: " << b.getX() << ", " << b.getY() << std::endl;
+	std::cout << "C values: " << c.getX() << ", " << c.getY() << std::endl;
+	std::cout << "P values: " << p.getX() << ", " << p.getY() << std::endl;
+
+	if (bsp(a, b, c, p))
+		std::cout << "Point is inside triangle!\n";
+	else
+		std::cout << "Point is outside triable\n";
 	return 0;
 }
