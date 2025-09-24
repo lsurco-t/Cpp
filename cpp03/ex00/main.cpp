0673f7bb0d158bc71a26e730b5f4d04e8227a79b
Lucio <lucio.surcotaype@outlook.com>
2025-09-24 20:17:52 +0300

    saved progress

--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -16,15 +16,18 @@ int main(void){
 	nameBot.attack("Enemy");
 	copyBot.attack("Enemy");
 
+	std::cout << "\n\033[31mClapTrap takes damage!\033[0m\n";
 	nameBot.takeDamage(5);
 	nameBot.takeDamage(10);
 
+	std::cout << "\n\033[31mTesting when ran out of points\033[0m\n";
 	copyBot.beRepaired(3);
 	copyBot.takeDamage(20);
 	copyBot.beRepaired(5);
 
+	std::cout << "\n\033[31mRapid attack 15 times!\033[0m\n";
 	for (int i = 0; i < 15; i++)
 		defaultBot.attack("anotherBot");
-		
+
 	return 0;
 }
