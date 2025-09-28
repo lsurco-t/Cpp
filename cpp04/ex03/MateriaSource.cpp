873f0899995841a7a24411bae9c58b0c457949b1
Lucio <lucio.surcotaype@outlook.com>
2025-09-28 20:49:23 +0300

    saved progress

--- cpp04/ex03/MateriaSource.cpp
+++ cpp04/ex03/MateriaSource.cpp
@@ -1,9 +1,48 @@
 #include "MateriaSource.hpp"
 
-MateriaSource::MateriaSource(){}
+MateriaSource::MateriaSource(){
+	for (int i = 0; i < 4; i++)
+		_materiaTypes[i] = nullptr;
+}
 
-MateriaSource::MateriaSource(const MateriaSource& other){}
+MateriaSource::MateriaSource(const MateriaSource& other){
+	for (int i = 0; i < 4; i++)
+		if(other._materiaTypes[i])
+			_materiaTypes[i] = other._materiaTypes[i]->clone();
+		else
+			_materiaTypes[i] = nullptr;
+}
 
-MateriaSource& MateriaSource::operator=(const MateriaSource other){}
+MateriaSource& MateriaSource::operator=(const MateriaSource& other){
+	if (this != &other){
+		for (int i = 0; i < 4; i++){
+			delete _materiaTypes[i];
+			if(other._materiaTypes[i])
+				_materiaTypes[i] = other._materiaTypes[i]->clone();
+			else
+				_materiaTypes[i] = nullptr;
+		}
+	}
+	return *this;
+}
 
-MateriaSource::~MateriaSource(){}
+MateriaSource::~MateriaSource(){
+	for (int i = 0; i < 4; i++)
+		delete _materiaTypes[i];
+}
+
+void MateriaSource::learnMateria(AMateria* m){
+	for (int i = 0; i < 4; i++)
+		if (_materiaTypes[i] == nullptr){
+			_materiaTypes[i] = m;
+			return ;
+		}
+}
+
+AMateria* MateriaSource::createMateria(std::string const & type){
+	for (int i = 0; i < 4; i++){
+		if (_materiaTypes[i] && _materiaTypes[i]->getType() == type)
+			return _materiaTypes[i]->clone();
+	}
+	return nullptr;
+}
--- cpp04/ex03/MateriaSource.hpp
+++ cpp04/ex03/MateriaSource.hpp
@@ -1,15 +1,19 @@
 #pragma once
 #include "AMateria.hpp"
 #include "IMateriaSource.hpp"
+#include <iostream>
 
-class MateriaSource{
+class MateriaSource : public IMateriaSource{
+
+	private:
+		AMateria* _materiaTypes[4];
 
 	public:
 		MateriaSource();
 		MateriaSource(const MateriaSource& other);
-		MateriaSource& operator=(const MateriaSource other);
+		MateriaSource& operator=(const MateriaSource& other);
 		~MateriaSource();
 
-		void learnMateria(AMateria*);
-		void createMateria(std::string const &);
+		void learnMateria(AMateria*) override;
+		AMateria* createMateria(std::string const & type) override;
 };
