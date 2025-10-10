30f4472221228708bd2a398720468ee77fcee955
Lucio <lucio.surcotaype@outlook.com>
2025-10-10 11:55:00 +0300

    save progress

--- cpp05/ex00/Bureaucrat.hpp
+++ cpp05/ex00/Bureaucrat.hpp
@@ -4,7 +4,16 @@
 class Bureaucrat {
 
 	private:
+		std::string _name;
+		unsigned int _grade;
 
 	public:
+		Bureaucrat(std::string name, unsigned int grade);
+		~Bureaucrat();
+
+		void setName(const std::string& name);
+		void setGrade(unsigned int grade);
+		const std::string& getName(void) const;
+		const std::string& getGrade(void) const;
 
 };
