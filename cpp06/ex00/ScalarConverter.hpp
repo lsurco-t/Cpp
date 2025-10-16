fbee967257952bcd62333b2b8389705b789688c2
Lucio <lucio.surcotaype@outlook.com>
2025-10-16 15:24:32 +0300

    saved progress

---
+++ cpp06/ex00/ScalarConverter.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+
+class ScalarConverter {
+
+	private:
+
+
+	public:
+
+	
+};
---
+++ cpp06/ex00/main.cpp
@@ -0,0 +1,7 @@
+#include "ScalarConverter.hpp"
+
+int main(void){
+
+	
+	return 0;
+}
