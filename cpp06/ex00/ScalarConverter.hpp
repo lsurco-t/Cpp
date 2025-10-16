77aef4ab9f49b12269499082c92a1ab4e5f6f909
Lucio <lucio.surcotaype@outlook.com>
2025-10-16 15:58:02 +0300

    saved progress

--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -3,10 +3,11 @@
 
 class ScalarConverter {
 
-	private:
-
-
 	public:
+		ScalarConverter();
+		ScalarConverter(const ScalarConverter& other);
+		ScalarConverter& operator=(const ScalarConverter& other);
+		~ScalarConverter();
 
-	
+		void convert(void) const;
 };
--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -1,6 +1,6 @@
 #include "ScalarConverter.hpp"
 
-int main(void){
+int main(int argc, char **argv){
 
 	
 	return 0;
