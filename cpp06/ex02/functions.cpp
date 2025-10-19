3a8ddcc9d7462773b5e497760cd4e1da6a600cf9
Lucio <lucio.surcotaype@outlook.com>
2025-10-19 18:45:59 +0300

    saved ex02

--- cpp06/ex02/functions.cpp
+++ cpp06/ex02/functions.cpp
@@ -23,9 +23,33 @@ Base * generate(void){
 }
 
 void identify(Base* p){
-
+	if (dynamic_cast<A*>(p))
+		std::cout << "Type of object is A" << std::endl;
+	else if (dynamic_cast<B*>(p))
+		std::cout << "Type of object is B" << std::endl;
+	else if (dynamic_cast<C*>(p))
+		std::cout << "Type of object is C" << std::endl;
 }
 
 void identify(Base& p){
+	try {
+		A&  a = dynamic_cast<A&>(p);
+		std::cout << "object is A" << std::endl;
+		return ;
+	} catch (std::bad_cast&){}
 
+	try {
+		B& b = dynamic_cast<B&>(p);
+		std::cout << "Object is B" << std::endl;
+		return ;
+
+	} catch (std::bad_cast&){}
+
+	try {
+		C& c = dynamic_cast<C&>(p);
+		std::cout << "Object is C" << std::endl;
+		return ;
+	} catch (std::bad_cast&){}
+
+	std::cout << "Unknown type" << std::endl;
 }
