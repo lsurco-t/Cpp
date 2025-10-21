1bc9d91bb2ee1b27db92c66e661f710d3e156c39
Lucio <lucio.surcotaype@outlook.com>
2025-10-21 20:24:11 +0300

    saved progress

--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -7,23 +7,39 @@
 //Template function to pass as argument
 template <typename T>
 void print(const T& c){
-	std::cout << c << std::endl;
+	std::cout << c << " ";
 }
 
 //Regular function passed as argument
 void increaseOne(const int& num){
-	std::cout << num + 1 << std::endl;
+	std::cout << num + 1;
 }
+
+//Function toUpper
+void toUpper(const char& c){
+	if (c >= 'a' && c <= 'z')
+		std::cout << (char)(c - 32);
+	else
+		std::cout << c ;
+}
+
 
 int main(void){
 
 	std::cout << GREEN "===== First test: Increase numbers by 1 =====" RESET << std::endl;
 	int arr[] = {1, 2, 3, 4};
 	::iter(arr, std::size(arr), increaseOne);
+	std::cout << "\n";
 
 	std::cout << GREEN "===== Second test: Print every word =====" RESET << std::endl;
 	std::string sentence[] = {"This", "is", "a", "sentence"};
 	::iter(sentence, std::size(sentence), print);
+	std::cout << "\n";
+
+	std::cout << GREEN "===== Third test: Print every letter =====" RESET << std::endl;
+	char word[] = "hello";
+	::iter(word, std::size(word), toUpper);
+	std::cout << "\n";
 
 	return 0;
 }
