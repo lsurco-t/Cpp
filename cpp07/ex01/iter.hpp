3f9bfab83dc35646611d23265894a0cf9c31ba3b
Lucio <lucio.surcotaype@outlook.com>
2025-10-21 20:03:14 +0300

    saved progress

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -1,7 +1,8 @@
 #pragma once
+#include <cstddef>
 
 template<typename T>
-void iter (T* address, size_t length, void (*func)(void)){
-	for (size_t i = 0; i  < length; i++)
-
+void iter (T* address, size_t length, void (*func)(const T&)){
+	for (size_t i = 0; i < length; i++)
+		func(address[i]);
 }
--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -1,8 +1,29 @@
 #include "iter.hpp"
 #include <iostream>
 
+#define GREEN "\033[32m"
+#define RESET "\033[0m"
+
+//Template function to pass as argument
+template <typename T>
+void print(const T& c){
+	std::cout << c << std::endl;
+}
+
+//Regular function passed as argument
+void increaseOne(const int& num){
+	std::cout << num + 1 << std::endl;
+}
+
 int main(void){
 
-	
+	std::cout << GREEN "===== First test: Increase numbers by 1 =====" RESET << std::endl;
+	int arr[] = {1, 2, 3, 4};
+	::iter(arr, std::size(arr), increaseOne);
+
+	std::cout << GREEN "===== Second test: Print every word =====" RESET << std::endl;
+	std::string sentence[] = {"This", "is", "a", "sentence"};
+	::iter(sentence, std::size(sentence), print);
+
 	return 0;
 }
