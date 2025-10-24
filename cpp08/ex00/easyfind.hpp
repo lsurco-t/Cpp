2f37dd8d99a14aec0aef5b96c072d90252be48e9
Lucio <lucio.surcotaype@outlook.com>
2025-10-24 19:35:09 +0300

    saved progress

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -3,10 +3,9 @@
 #include <algorithm>
 
 template<typename T>
-typename T::const_iterator easyfind(const T& container, int value){
-	auto it = std::find(container.begin(), container.end(), value);
-	if (it != container.end())
-		return it;
-	else
+typename T::iterator easyfind(T& container, int value){
+	typename T::iterator it = std::find(container.begin(), container.end(), value);
+	if (it == container.end())
 		throw std::runtime_error("Element not found");
+	return it;
 }
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -1,6 +1,8 @@
 #include "easyfind.hpp"
 #include <iostream>
 #include <list>
+#include <vector>
+#include <deque>
 
 #define RED "\033[31m"
 #define GREEN "\033[32m"
@@ -8,20 +10,50 @@
 
 int main(void){
 
-	std::cout << GREEN "===== First test: Valid number to find (3) =====" RESET << std::endl;
+	std::cout << GREEN "===== First test: Valid number to find =====" RESET << std::endl;
 	std::list<int> firstList = {1, 2, 3, 4, 5};
 	try{
-		auto it = ::easyfind(firstList, 3);
-		std::cout << "The value searched is: " << *it << std::endl; 
-	}catch(std::exception &e){
-		std::cout << "Error: " << std::endl;
+		std::cout << "Elements of list: ";
+		for (const int& n : firstList)
+			std::cout << n << " ";
+		std::cout << std::endl;
+		std::list<int>::iterator it = ::easyfind(firstList, 3);
+		std::cout << "The value found is: " << *it << std::endl; 
+	}catch(const std::exception &e){
+		std::cout << "Error: " << e.what() << std::endl;
 	}
 
-	std::cout << GREEN "===== Second test: Invalid number to find (10) =====" RESET << std::endl;
+	std::cout << GREEN "\n===== Second test: Invalid number to find =====" RESET << std::endl;
 	try{
-		auto it = ::easyfind(firstList, 10);
-		std::cout << "The value search is: " << *it << std::endl; 
-	}catch(std::exception &e){
+		std::list<int>::iterator it = ::easyfind(firstList, 10);
+		std::cout << "The value found is: " << *it << std::endl; 
+	}catch(const std::exception &e){
+		std::cout << RED "Error: " << e.what() << std::endl;
+	}
+
+	std::cout << GREEN "\n===== Third test: Valid number in vector =====" RESET << std::endl;
+	std::vector<int> vectorOne = {10, 12, 20, 25};
+	try{
+		std::cout << "Elements of vector: ";
+		for (unsigned int i = 0; i < std::size(vectorOne); i++)
+			std::cout << vectorOne.at(i) << " ";
+		std::cout << std::endl;
+		std::vector<int>::iterator it = ::easyfind(vectorOne, 20);
+		std::cout << "The value found is: " << *it << std::endl; 
+	}catch(const std::exception &e){
+		std::cout << RED "Error: " << e.what() << std::endl;
+	}
+
+	std::cout << GREEN "\n===== Fourth test: Valid number in deque =====" RESET << std::endl;
+	std::deque<int> dequeOne = {1, 3, 5, 7, 9, 11, 13, 15};
+	try{
+		std::cout << "Elements of deque: ";
+		for (unsigned int i = 0; i < std::size(dequeOne); i++)
+			std::cout << dequeOne.at(i) << " ";
+		std::cout << std::endl;
+		std::deque<int>::iterator it = ::easyfind(dequeOne, 11);
+		std::cout << "The value found is: " << *it << std::endl; 
+	}catch(const std::exception &e){
 		std::cout << RED "Error: " << e.what() << std::endl;
 	}
 
