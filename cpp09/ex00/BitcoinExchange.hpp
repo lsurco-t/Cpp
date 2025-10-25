103bcae03c76055b5ee1e0fc61f52651a020c38e
Lucio <lucio.surcotaype@outlook.com>
2025-10-25 22:54:05 +0300

    saved files

---
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <algorithm>
+
+class BitcoinExchange{
+
+	private:
+
+	public:
+
+};
---
+++ cpp09/ex00/main.cpp
@@ -0,0 +1,7 @@
+#include "BitcoinExchange.hpp"
+#include <iostream>
+
+int main(int argc, char **argv){
+
+	return 0;
+}
