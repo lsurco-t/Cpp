b88d92bf9f924a3447dca572b10ce2260eea0916
Lucio <lucio.surcotaype@outlook.com>
2026-01-08 11:59:29 +0200

    saved progress

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -1,1 +1,55 @@
 #include "BitcoinExchange.hpp"
+
+enum INVALID_FILE {
+	IS_EMPTY,
+	IS_DIR,
+	NOT_EXIST,
+	WRONG_EXT,
+
+};
+
+bool BitcoinExchange::isValidFile(std::string& file){
+	std::error_code ec;
+	if (!std::filesystem::exists(file, ec)){
+		return returnMessage(NOT_EXIST, file);
+	}
+	else if (std::filesystem::path(file).extension() != ".txt"){
+		return returnMessage(WRONG_EXT, file);
+	}
+	else if (std::filesystem::is_directory(file, ec)){
+		return returnMessage(IS_DIR, file);
+	}
+	else if (std::filesystem::is_empty(file, ec)){
+		return returnMessage(IS_EMPTY, file);
+	}	
+	return true;
+}
+
+unsigned int BitcoinExchange::matchPrice(std::string_view input, std::string_view database){
+
+}
+
+void BitcoinExchange::displayOutput(std::string_view input, unsigned int price){
+
+}
+
+bool returnMessage(unsigned int reason, std::string& file){
+	std::string reasonStr;
+	switch(reason){
+		case IS_EMPTY:
+			reasonStr = " is empty\n";
+			break;
+		case IS_DIR:
+			reasonStr = " is a directory\n";
+			break;
+		case NOT_EXIST:
+			reasonStr = " does not exist\n";
+			break;
+		case WRONG_EXT:
+			reasonStr = " has wrong extension\n";
+			break;
+	}
+	std::cout << "File " << file << reasonStr;
+	return false;
+}
+
--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -1,13 +1,21 @@
 #pragma once
 #include <algorithm>
+#include <iostream>
+#include <filesystem>
+#include <map>
 
 class BitcoinExchange{
 
 	private:
-		
+		std::map<int, int> _trades;
+
 	public:
-		BitcoinExchange();
-		BitcoinExchange(const BitcoinExchange& other);
-		BitcoinExchange& operator=(const BitcoinExchange& other);
-		~BitcoinExchange();
+		BitcoinExchange() = default;
+		BitcoinExchange(const BitcoinExchange& other) = default;
+		BitcoinExchange& operator=(const BitcoinExchange& other) = default;
+		~BitcoinExchange() = default;
+
+		bool 			isValidFile(std::string& file);
+		unsigned int	matchPrice(std::string_view input, std::string_view database);
+		void 			displayOutput(std::string_view input, unsigned int price);
 };
