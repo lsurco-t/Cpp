8ff25ca4f3d8ec1c4be1accb55ed4ad631d7f0f3
Lucio <lucio.surcotaype@outlook.com>
2026-01-19 12:50:44 +0200

    saved progress

--- cpp09/ex01/RPN.hpp
+++ cpp09/ex01/RPN.hpp
@@ -1,12 +1,16 @@
 #pragma once
+#include <stack>
 
 class RPN {
 	
 	private:
-		
+		std::stack<int, int> _numbers;
+
 	public:
 		RPN();
 		RPN(const RPN& other);
 		RPN& operator=(const RPN& other);
 		~RPN();
+
+		bool isValidCharacter();
 };
--- cpp09/ex01/main.cpp
+++ cpp09/ex01/main.cpp
@@ -1,1 +1,10 @@
 #include "RPN.hpp"
+
+// General error codes return
+constexpr int FAILURE = 1;
+constexpr int SUCCESS = 0;
+
+int main(int argc, char **argv){
+	
+	return FAILURE;
+}
