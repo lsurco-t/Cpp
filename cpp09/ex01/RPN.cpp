c1611632c2fd1be5a89d1ed9a7bb273d7073b535
Lucio <lucio.surcotaype@outlook.com>
2026-01-19 15:20:00 +0200

    changed order of numbers to calculate

--- cpp09/ex01/RPN.cpp
+++ cpp09/ex01/RPN.cpp
@@ -2,16 +2,16 @@
 
 bool RPN::isValidCharacter(const char& chr){
 	if ((chr == '+' || chr == '-' || chr == '/' ||
-		chr == '*' || chr == 32) || (chr >= '0' && chr <= '9')){
+		chr == '*' || chr == ' ') || (chr >= '0' && chr <= '9')){
 		return true;
 	}
 	return false;
 }
 
 int RPN::calculateRPN(const std::string& args){
-	int firstNum;
-	int secondNum;
-	int result;
+	int firstNum = 0;
+	int secondNum = 0;
+	int result = 0;
 
 	for (auto n : args){
 		if (!isValidCharacter(n)){
@@ -22,13 +22,13 @@ int RPN::calculateRPN(const std::string& args){
 			int number = std::atoi(&n);
 			_numbers.push(number);
 		} else if (n != ' '){
-			firstNum = _numbers.top();
+			secondNum = _numbers.top();
 			_numbers.pop();
 			if (_numbers.size() < 1){
-				std::cerr << "Not enough numbers to calculate value";
+				std::cerr << "Not enough numbers to calculate value\n";
 				return FAILURE;
 			}
-			secondNum = _numbers.top();
+			firstNum = _numbers.top();
 			_numbers.pop();
 			switch (n){
 				case '*':
