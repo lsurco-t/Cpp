48dce0c54b190d2ada193899695a8624d6ee41b0
Lucio <lucio.surcotaype@outlook.com>
2026-01-20 12:25:30 +0200

    saved progress on ex03

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -1,2 +1,13 @@
 #include "PmergeMe.hpp"
 
+bool PmergeMe::isValidSequence(int argc, const char **argv, std::vector<int> input){
+	
+}
+
+void PmergeMe::fordJohnsonVect(std::vector<int>& vect){
+
+}
+
+void PmergeMe::fordJohnsonDeq(std::deque<int>& deq){
+
+}
--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -1,12 +1,27 @@
 #pragma once
+#include <deque>
+#include <vector>
+#include <iostream>
+#include <string>
+
+// General error codes return
+constexpr int FAILURE = 1;
+constexpr int SUCCESS = 0;
 
 class PmergeMe {
 
 	private:
+		std::vector<int> _vect;
+		std::deque<int>	_deq;
 
 	public:
-		PmergeMe();
-		PmergeMe(const PmergeMe& other);
-		PmergeMe& operator=(const PmergeMe other);
-		~PmergeMe();
+		PmergeMe() = default;
+		PmergeMe(const PmergeMe& other) = delete;
+		PmergeMe& operator=(const PmergeMe other) = delete;
+		~PmergeMe() = default;
+
+		bool isValidSequence(int argc, const char **argv, std::vector<int> input);
+		void fordJohnsonVect(std::vector<int>& vect);
+		void fordJohnsonDeq(std::deque<int>& deq);
+
 };
